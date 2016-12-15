d87a4bf4826764244eb77704f85c2f466680fa92
koka <kai@greetingworks.com>
2016-12-15 19:43:48 +0900

    new gh-pages version

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{419:function(n,o){},762:function(n,o,c){n.exports=c(419)}},[762]);
+webpackJsonp([1,2],{421:function(n,o){},764:function(n,o,c){n.exports=c(421)}},[764]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
