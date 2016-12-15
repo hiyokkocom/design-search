fc7d99497cae156b53a5f3477fb4f4b51a5b936e
koka <kai@greetingworks.com>
2016-12-15 14:20:17 +0900

    - Masonryを導入した。(#3)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,24 @@ export class AppComponent {
   // ngAfterViewInit() {
   //   this.tagInput.focus();
   // }
+
+  tags = [
+    { icon: "label", title: "イラスト" },
+    { icon: "label_outline", title: "写真" },
+    { icon: "label_outline", title: "シンプル" },
+    { icon: "label_outline", title: "ねこ" },
+    { icon: "label_outline", title: "写真が2点" },
+    { icon: "label_outline", title: "写真が3点" }
+  ];
+
+  items = [
+    "http://nenga.aisatsujo.jp/images/items/N17C001b.jpg",
+    "http://nenga.aisatsujo.jp/images/items/N17C002b.jpg",
+    "http://nenga.aisatsujo.jp/images/items/N17C003b.jpg",
+    "http://nenga.aisatsujo.jp/images/items/N17C004b.jpg",
+    "http://nenga.aisatsujo.jp/images/items/N17C005b.jpg",
+    "http://nenga.aisatsujo.jp/images/items/N17C006b.jpg",
+    "http://nenga.aisatsujo.jp/images/items/N17C007b.jpg",
+    "http://nenga.aisatsujo.jp/images/items/N17C008b.jpg",
+  ];
 }
